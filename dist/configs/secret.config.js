2c856748f3d6e7fd1ce0de4a080570c58c58bf6e
M Fatkhurohman Akbar <akbar@hubble.sg>
2023-05-25 00:05:58 +0700

    a

--- dist/configs/secret.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var secret = "ADALAHKAMUSATU-SATUNYA123";
-var _default = secret;
-exports["default"] = _default;
--- dist/configs/whitelist.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.whiteList = void 0;
-var whiteList = ['http://localhost:3000'];
-exports.whiteList = whiteList;
